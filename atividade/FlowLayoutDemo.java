3a8bc5b1e86182b3411c6e7cd2dffda8f5ecf083
manojaya <124295549+renanjaya@users.noreply.github.com>
2023-05-11 14:31:07 -0400

    Add files via upload

---
+++ atividade/FlowLayoutDemo.java
@@ -0,0 +1,13 @@
+package atividade;
+
+import javax.swing.JFrame;
+
+public class FlowLayoutDemo {
+
+    public static void main(String[] args) {
+        FlowLayoutFrame flowLayoutFrame = new FlowLayoutFrame();
+        flowLayoutFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        flowLayoutFrame.setSize(300, 75); // configura o tamanho do frame
+        flowLayoutFrame.setVisible(true); // exibe o frame
+    } // fim de main
+} // fim da classe FlowLayoutFrame
